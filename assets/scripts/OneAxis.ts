8da1411a0bca52cbb38d2b8856487ea52dad9188
jacobkim9881 <jacobkim9881@gmail.com>
2022-03-28 16:17:17 +0900

    test: added status of each move keys

---
+++ assets/scripts/OneAxis.ts
@@ -0,0 +1,25 @@
+
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = OneAxis
+ * DateTime = Fri Mar 25 2022 15:37:12 GMT+0900 (대한민국 표준시)
+ * Author = jacobkim9881
+ * FileBasename = OneAxis.ts
+ * FileBasenameNoExtension = OneAxis
+ * URL = db://assets/scripts/OneAxis.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/en/
+ *
+ */
+ 
+@ccclass('OneAxis')
+export class OneAxis extends Component {
+    isPushed = 0
+    pushingTime = 0
+    pausedTime = 0
+    overedPushingTime = 0.01
+    overedPausedTime = 0.04
+    position = 0    
+}
--- assets/scripts/Player.ts
+++ assets/scripts/Player.ts
@@ -1,5 +1,6 @@
 
 import { _decorator, Component, Node, systemEvent, SystemEvent, SystemEventType, EventKeyboard, Vec3, KeyCode } from 'cc';
+import { OneAxis } from './OneAxis'
 const { ccclass, property } = _decorator;
 
 /**
@@ -22,6 +23,21 @@ export class Player extends Component {
 
     private _curPos = new Vec3();
     private _tarPos = new Vec3();
+    private _keycode = 0;
+    private moveLen = 3;
+
+    private _x = new OneAxis();
+    private _y = new OneAxis();
+    private _z = new OneAxis();
+
+    private sadis = {
+        isPushed: 0,
+        pushingTime: 0,
+        pausedTime: 0,
+        overedPushingTime: 0.1,
+        overedPausedTime: 0.1,
+        position: 0
+    }
     private _isPushed = 0;
     private pushingTime = 0;
     private pausedTime = 0;
@@ -30,10 +46,8 @@ export class Player extends Component {
     private _xCode = 0;
     private _zCode = 0;
     private _yCode = 0;
-    private _keycode = 0;
-    private moveLen = 3;
 
-    start () {
+    start () {        
       systemEvent.on(SystemEventType.KEY_DOWN, this.onKeyDown, this);
       systemEvent.on(SystemEventType.KEY_UP, this.onKeyUP, this);        
         // [3]
@@ -46,32 +60,40 @@ export class Player extends Component {
     onKeyDown(e: EventKeyboard) {
         console.log('key pushed: ', e.keyCode)
         this._keycode = e.keyCode;
-        this._isPushed = 1;      
-        this.pausedTime = 0;
 
-        this._xCode = this._keycode === 37 ? - this.moveLen 
-        : this._keycode === 39 ? this.moveLen 
-        : this._xCode;
-
-        this._zCode = this._keycode === 38 ? - this.moveLen 
-        : this._keycode === 40 ? this.moveLen 
-        : this._zCode;
+        if (e.keyCode === 37 || e.keyCode === 39) {
+            this._x.isPushed = 1;
+            this._x.pausedTime = 0
+            this._x.position = e.keyCode === 37 ? - this.moveLen 
+        : e.keyCode === 39 ? this.moveLen 
+        : this._x.position;
+        } else if (e.keyCode === 38 || e.keyCode === 40) {
+            this._z.isPushed = 1;
+            this._z.pausedTime = 0
+            this._z.position = e.keyCode === 38 ? - this.moveLen 
+        : e.keyCode === 40 ? this.moveLen 
+        : this._z.position;
+        }
         
     }
 
     onKeyUP(e: EventKeyboard) {
         //console.log('key up: ', e.keyCode)
-        this._keycode = e.keyCode;
-        this._isPushed = 0;                
-        this.pushingTime = this.pausedTime <= 0.2 ? this.pushingTime : 0;
+        if (e.keyCode === 37 || e.keyCode === 39) {               
+            this._x.isPushed = 0;
+            this._x.pushingTime = this._x.pausedTime <= 0.04 ? this._x.pushingTime : 0;
+            this._x.position = 0;
+        } else if (e.keyCode === 38 || e.keyCode === 40) {
+            this._z.isPushed = 0;
+            this._z.pushingTime = this._z.pausedTime <= 0.2 ? this._z.pushingTime : 0;
+            this._z.position = 0;
 
-        if (this._keycode === 37 || this._keycode === 39) this._xCode = 0;        
-        if (this._keycode === 38 || this._keycode === 40) this._zCode = 0;
+        }
     }
 
-    moveObj(x: number, y: number, z: number, dt: number) {                 
-        x = x * dt;
-        z = z * dt;        
+    moveObj(x: number, y: number, z: number, xdt: number, ydt: number, zdt: number) {                 
+        x = x * xdt;
+        z = z * zdt;        
         //console.log(x !== 0 ? x : null + z !== 0 ? z :null)
         //console.log(x)
         //console.log(this.node.getPosition(this._curPos))       
@@ -82,6 +104,7 @@ export class Player extends Component {
     }
 
     calPushTime(pushingTime: number, overed: number, dt: number, pushed: number) {
+        //return pushingTime = pushingTime + dt * 0.3 * pushed;
         return pushingTime = pushingTime <= overed ? pushingTime + dt * 0.3 * pushed : pushingTime;
     }
 
@@ -90,9 +113,12 @@ export class Player extends Component {
     }
 
     update(dt: number) {        
-        this.moveObj(this._xCode, this._yCode, this._zCode, this.pushingTime);        
-        this.pushingTime = this.calPushTime(this.pushingTime, this.overedPushingTime, dt, this._isPushed);
-        this.pausedTime = this.calPausedTime(this.pausedTime, this.overedPausedTime, dt, this._isPushed);        
+        this.moveObj(this._x.position, this._y.position, this._z.position, this._x.pushingTime, this._y.pushingTime, this._z.pushingTime);        
+        this._x.pushingTime = this.calPushTime(this._x.pushingTime, this._x.overedPushingTime, dt, this._x.isPushed);
+        this._x.pausedTime = this.calPausedTime(this._x.pausedTime, this._x.overedPausedTime, dt, this._x.isPushed);        
+        console.log(this._x.pushingTime, this._x.pausedTime)
+        //this._x.pushingTime = this.calPushTime(this._x.pushingTime, this._x.overedPushingTime, dt, this._x.isPushed);
+        this._z.pushingTime = this.calPushTime(this._z.pushingTime, this._z.overedPushingTime, dt, this._z.isPushed);
         
     }
 
