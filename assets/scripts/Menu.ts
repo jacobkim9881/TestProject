138922a3eecf5715b9d1ef65c3b5788003118430
jacobkim9881 <jacobkim9881@gmail.com>
2022-04-19 18:23:09 +0900

    test: added menu page script

--- assets/scripts/Menu.ts
+++ assets/scripts/Menu.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, Label } from 'cc';
+import { _decorator, Component, Node, Label, assetManager, Button, JsonAsset, systemEvent, SystemEventType, EventMouse } from 'cc';
 const { ccclass, property } = _decorator;
 
 /**
@@ -20,10 +20,62 @@ export let labels:Array<any> = null!
 export class Menu extends Component {    
     private _label2:Array<object> = null!
 
-    start () {
-        labels = this.node.getComponentsInChildren(Label);        
-        console.log(labels)
-        //this._label2 = this.node.getChildByName('guide');
+    private _guideCon = null!
+    private _pageNum = null!
+    private _prev_button = null!
+    private _next_button = null!
+    private _returns = null!
+
+    async start () {
+        //this.onStart();
+
+        systemEvent.on(SystemEventType.MOUSE_DOWN, this.onMouseDown, this);
+        
+        let butts = this.getComponentsInChildren(Button);
+            labels = this.getComponentsInChildren(Label);            
+            this._guideCon = labels[4];
+            this._pageNum = labels[1];
+            this._prev_button = labels[2];
+            this._next_button = labels[3];
+            this._returns = labels[5];
+
+            let gameGuide;
+    console.log(butts)
+    console.log(labels)
+    console.log(this._guideCon)
+        gameGuide = await assetManager.loadAny({uuid: '02eb9734-d497-423b-a150-618868fe8981'}, JsonAsset, (err, data) => {
+            this._guideCon.string = data.json.page[0].guide;
+            this._pageNum.string = `1/${data.json.page.length}`
+            this._returns.string = data.json.page[0].returns
+            this.menuButtonEvent(butts, data, 1);
+            this.menuButtonEvent(butts, data, 0);
+        
+            
+            console.log(data)
+            if (err) console.log(err);
+            return data.json    
+        });        
+
+        return;
+    }
+
+    menuButtonEvent(butts:Array<any>, data, pageNum) {        
+    let next = pageNum === 1 ? -1 : 1;            
+    let buttonNum = pageNum === 1 ? 0 : 1;            
+    let getPage = pageNum === 1 ? -2 : -1;            
+    butts[buttonNum].node.on('click', (e) =>{
+        let curPage = parseInt(this._pageNum.string.split('/')[0]);
+        if (curPage > pageNum && curPage < data.json.page.length && curPage !==1) {
+            this._pageNum.string = `${curPage + next}/${data.json.page.length}`
+            this._guideCon.string = data.json.page[curPage - getPage].guide;
+            this._returns.string = data.json.page[curPage - getPage].returns
+        }
+        console.log('cliked')
+    }, this)
+    }
+
+    onMouseDown(e: EventMouse) {
+        console.log(e)
     }
 
 }
