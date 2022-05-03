2e2b20f62bf1e68e2035110c8f9bdd9d43601f74
jacobkim9881 <jacobkim9881@gmail.com>
2022-05-03 12:18:04 +0900

    test: collider for ball obj

--- assets/scripts/Ball.ts
+++ assets/scripts/Ball.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, CapsuleCollider, SphereCollider } from 'cc';
 const { ccclass, property } = _decorator;
 
 /**
@@ -13,10 +13,23 @@ const { ccclass, property } = _decorator;
  * ManualUrl = https://docs.cocos.com/creator/3.4/manual/en/
  *
  */
+
+export let isContacted
  
 @ccclass('Ball')
 export class Ball extends Component {
 
     start () {
+let _collider = this.getComponent(SphereCollider)
+    console.log(_collider)
+    _collider.on('onCollisionEnter', this.collisionEnter, this)
     }
+    
+    collisionEnter(e) {
+        console.log('collided: ',e)
+        console.log(e.otherCollider)
+        console.log(e.otherCollider.node.name)
+        return
+      }
+
 }
